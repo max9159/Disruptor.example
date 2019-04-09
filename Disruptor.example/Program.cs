dc9835a936902d4029c353a5cb68154240f5852d
Max Chou <max.chou@xuenn.com>
2019-04-09 18:55:29 +0800

    Add Simulator Class for Simulate Publish Events.

--- Disruptor.example/Program.cs
+++ Disruptor.example/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -10,44 +11,33 @@ namespace Disruptor.basic
 {
     class Program
     {
-        private static readonly Random _random = new Random();
         private const int _ringSize = 16;  // Must be multiple of 2
         static void Main(string[] args)
         {
+            var sw = new Stopwatch();
+            sw.Start();
+
             var disruptor = new Disruptor.Dsl.Disruptor<MyValueEntry>(() => new MyValueEntry(), _ringSize, System.Threading.Tasks.TaskScheduler.Default);
 
-            //1 Producer - 2 Consumer (Steps Pipeline)
+            //Case A: 1 Producer - 2 Consumer (Steps Pipeline)
             disruptor.HandleEventsWith(
                 new MyValueAdditionHandler()).Then(
                     new MyLogHandler(@"C:\Disruptor.basic.log"));
-            ////1 Producer - 2 Consumer (multicast event)
+
+            ////Case B: 1 Producer - 2 Consumer (multicast event)
             //disruptor.HandleEventsWith(
             //    new MyValueAdditionHandler(),
             //    new MyLogHandler(@"C:\Disruptor.basic.log"));
 
             var ringBuffer = disruptor.Start();
 
-            while (true)
-            {
-                long sequenceNo = ringBuffer.Next();
-                MyValueEntry entry;
-                try
-                {
+            Simulator.SimulatePublishEvent(ringBuffer, 1000);
 
-                    entry = ringBuffer[sequenceNo];
+            sw.Stop();
 
-                    entry.Value = _random.Next();
-                }
-                finally
-                {
-                    //make sure the event will be sent.
-                    ringBuffer.Publish(sequenceNo);
-                }
+            Console.WriteLine($"elapsed {sw.ElapsedMilliseconds} ms");
+            Console.Read();
 
-                Console.WriteLine("Published entry {0}, value {1}", sequenceNo, entry.Value);
-
-                Thread.Sleep(250);
-            }
         }
     }
 }
---
+++ Disruptor.example/Simulator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Disruptor.basic;
+
+namespace Disruptor.example
+{
+    public class Simulator
+    {
+        private static readonly Random _random = new Random();
+        public static void SimulatePublishEvent(RingBuffer<MyValueEntry> ringBuffer, long simulateCount)
+        {
+            while (true)
+            {
+                long sequenceNo = ringBuffer.Next();
+                MyValueEntry entry;
+                try
+                {
+                    entry = ringBuffer[sequenceNo];
+                    entry.Value = _random.Next();
+                }
+                finally
+                {
+                    //make sure the event will be sent.
+                    ringBuffer.Publish(sequenceNo);
+                }
+
+                Console.WriteLine("Published entry {0}, value {1}", sequenceNo, entry.Value);
+                if (sequenceNo == simulateCount)
+                    break;
+            }
+        }
+    }
+}
